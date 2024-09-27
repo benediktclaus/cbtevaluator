b19aebdf82f833e6f3a84453318285260cb360e0
Benedikt Claus <benediktclaus@msn.com>
2024-09-27 07:03:51 +0200

    Simplify invitation batch file

--- inst/scripts/einladung-versenden.bat
+++ inst/scripts/einladung-versenden.bat
@@ -1,6 +1,3 @@
-R CMD BATCH "03 Einladung"\einladung-script.R
-if exist "03 Einladung"\protocol.txt del "03 Einladung"\protocol.txt
-ren "03 Einladung"\einladung-script.Rout protocol.txt
-del .Rdata
+Rscript "03 Einladung"\einladung-script.R
 
 pause
