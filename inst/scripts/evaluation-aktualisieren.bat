9b69eaa7c3c0fc324a12522b49ca9671dbb91c5f
Benedikt Claus <benediktclaus@msn.com>
2024-10-30 15:42:18 +0100

    Hotfix evaluation script

--- inst/scripts/evaluation-aktualisieren.bat
+++ inst/scripts/evaluation-aktualisieren.bat
@@ -1,7 +1,4 @@
 Rscript "~config"\evaluate-all-patients.R
-if exist "~config"\protocol.txt del "~config"\protocol.txt
-ren "~config"\evaluate-all-patients.Rout protocol.txt
-del .Rdata
 del Rplots.pdf
 
 pause
